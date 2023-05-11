ff73d36c698cec0213e047838e06884beb63fb12
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2023-05-10 21:30:01 -0500

    clean up files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3200;
+const PORT = process.env.PORT || 3000;
 
 //connect to MongoDB
 connectDB();
